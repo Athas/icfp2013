53b685810abf7674b2cae83bdfc015813cd5eab0
Niels G. W. Serup <ngws@metanohi.name>
2013-08-09 14:55:58 +0200

    added training data generator

---
+++ src/generate_trains.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import subprocess
+import time
+
+
+def generate_train():
+    try:
+        xs = subprocess.Popen(['./src/api.py', 'train'],
+                              stdout=subprocess.PIPE).communicate()[0].split('\n')
+        if xs[0] == 'json':
+            return xs[1]
+    except Exception:
+        return
+
+def generate_trains():
+    while True:
+        for i in range(0, 5):
+            print generate_train()
+        time.sleep(21)
+
+if __name__ == '__main__':
+    generate_trains()
