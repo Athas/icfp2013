10ef11ada2d15567485bb2bdeb355195408abb5b
Niels G. W. Serup <ngws@metanohi.name>
2013-08-09 21:26:00 +0200

    command.py now supports genetic program

--- src/command.py
+++ src/command.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import random
 import subprocess
 import api
 import json
-import numpy
+from multiprocessing import Process
 
 
 def load_problems(path):
@@ -60,6 +61,46 @@ def solve(auth, id):
             outputs.extend(j['outputs'])
 
     maps = zip(map(unhex, arguments), map(unhex, outputs))
+
+    troels = Process(target=run_troels, args=(auth, id, size, ops, maps))
+    genetic = Process(target=run_genetic, args=(auth, id, size, ops, arguments, outputs))
+    troels.start()
+    genetic.start()
+
+def format_c_array(xs):
+    return '{' + ', '.join(xs) + '}'
+    
+def run_genetic(auth, id, size, ops, arguments, outputs):
+    ops = set(ops)
+    if 'tfold' in ops:
+        ops.add('fold')
+        ops.remove('tfold')
+    if 'fold' in ops:
+        ops.add('acc')
+        ops.add('byte')
+    ops.add('zero')
+    ops.add('one')
+    ops.add('arg')
+    ops_arr = format_c_array(map(lambda op: op[0].upper() + op[1:], ops))
+    values_arr = format_c_array(arguments)
+    results_arr = format_c_array(map(lambda x: x.encode(), outputs))
+    data = '''\
+#define PROGSIZE %d
+term_t ok[] = %s;
+uint64_t test_values[]  = %s;
+uint64_t test_results[] = %s;
+#define RETRY_TIME 10
+    ''' % (size, ops_arr, values_arr, results_arr)
+    print 'data.h:\n'
+    print data
+    with open('src/data.h', 'w') as f:
+        f.write(data)
+    subprocess.Popen(['make', '-C', 'src'])
+    out = subprocess.Popen(['./src/genetic'], stdout=subprocess.PIPE).communicate()[0]
+    prog = out.strip()
+    guess_program(auth, id, prog, 'GENERNE')
+
+def run_troels(auth, id, size, ops, maps):
     cmd = ['./src/Main', 'solve', str(size), str(ops).replace("'", '"'), str(maps).replace('L', '')]
     print 'Command:', cmd
     out, err = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -71,6 +112,9 @@ def solve(auth, id):
         print 'NO SOLUTION FOUND!  TIME IS RUNNING OUT!'
         return
     prog = out.strip()
+    guess_program(auth, id, prog, 'TROELS')
+
+def guess_program(auth, id, prog, source):
     print 'Program found: ' + prog
     (t, d) = api.guess(auth, id, prog)
     if t == 'error':
@@ -81,9 +125,10 @@ def solve(auth, id):
         print d
         j = json.loads(d)
         if j['status'] == 'win':
-            print 'YOU WON! ' * 10
+            print (source + ' WON! ') * 10
             with open('wins', 'w') as f:
                 f.write(id + '\n')
+            sys.exit()
 
 def print_help():
     print '''usage: ./command.py COMMAND [ARGS] -> STATUS
